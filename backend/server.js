dd9ff05ef3a621127a7257c56ef9ee496e826162
Sathyapriya Sakthivel <sathyasakthi569@gmail.com>
2025-12-27 12:16:50 +0530

    Secure backend using environment variables

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const bookingRoutes = require("./routes/bookingRoutes");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
